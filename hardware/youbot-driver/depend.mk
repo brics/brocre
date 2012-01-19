dcb0c47f1e056cfa83629f6184558a4217e9416e
Jan Paulus <jan.paulus@h-brs.de>
2012-01-19 13:18:49 +0100

    first version with robotpkg and rosmake

---
+++ hardware/youbot-driver/depend.mk
@@ -0,0 +1,23 @@
+# robotpkg depend.mk for:	youbot-driver
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+YOUBOT_DRIVER_DEPEND_MK:= ${YOUBOT_DRIVER_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		youbot-driver
+endif
+
+ifeq (+,$(YOUBOT_DRIVER_DEPEND_MK))
+PREFER.youbot-driver?=	robotpkg
+
+DEPEND_USE+=		youbot-driver
+
+DEPEND_ABI.youbot-driver?=	youbot-driver>=0.9
+DEPEND_DIR.youbot-driver?=	../../youbot/youbot-driver
+
+SYSTEM_SEARCH.youbot-driver=\
+	youbot_driver/manifest.xml
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
