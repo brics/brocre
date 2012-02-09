c7e59e125bccd2eb4f29093cb4ddd7309fa01ea0
Jan Paulus <jan.paulus@h-brs.de>
2012-02-09 10:50:24 +0100

    remove only files which have been created in the brocre_package_tools.py

--- pkgtools/brocre_tools/brocre_package_tools.py
+++ pkgtools/brocre_tools/brocre_package_tools.py
@@ -47,7 +47,7 @@ def parsePKGMakefile(filename,currentpackage):
         if not COMMENT_IN_MAKEFILE in line and "LICENSE" in line and currentpackage.license == "":
             currentpackage.license = line[line.find("=")+1:].strip()
             
-	#filehandle.close()
+    #filehandle.close()
 
 
 def extractPackageDescriptions():
@@ -91,112 +91,112 @@ def parseRobotpkgInfo(inputtext,currentpackage):
 
 def generateBrocreFiles(packageName, categorie, masterSite, version, dependencies, brocrefolder):
 
-	try:
-		package = rospkg.RosStack()
-		package.get_manifest(packageName)
-		fileToCheckIfInstall = packageName + "/stack.xml"
-	#	version = package.get_manifest(packageName).version
-	except:
-		try:
-			package = rospkg.RosPack()
-			package.get_manifest(packageName)
-			fileToCheckIfInstall = packageName + "/manifest.xml"
-		except:
-			print "it is not a ros stack or package"
-			exit(0)
-	
-
-	folderName = package.get_path(packageName)
-	tarFileName = packageName +"-"+ version+ ".tar.gz"
-	
-	if not os.path.exists(brocrefolder+'/'+packageName):
-		os.makedirs(brocrefolder+'/'+packageName)
+    try:
+        package = rospkg.RosStack()
+        package.get_manifest(packageName)
+        fileToCheckIfInstall = packageName + "/stack.xml"
+    #    version = package.get_manifest(packageName).version
+    except:
+        try:
+            package = rospkg.RosPack()
+            package.get_manifest(packageName)
+            fileToCheckIfInstall = packageName + "/manifest.xml"
+        except:
+            print "it is not a ros stack or package"
+            exit(0)
     
-	print "create Makefile file"
-	makefile = open(brocrefolder+'/'+packageName+'/Makefile', 'w')
-	versionstring = "PACKAGE_VERSION"
-	makefile.write(versionstring +" = "+ version+"\n")
-	makefile.write("DISTNAME = "+ packageName+"-${"+versionstring+"}\n")
-	makefile.write("PKGNAME = "+ packageName+"-${"+versionstring+"}\n")
-	makefile.write("CATEGORIES = " + categorie + "\n")
-	makefile.write("MASTER_SITES = "+ masterSite+"\n")
-	makefile.write("MASTER_REPOSITORY = \n\n")
 
-	makefile.write("MAINTAINER = " + package.get_manifest(packageName).author +"\n")
-	makefile.write("HOMEPAGE = " + package.get_manifest(packageName).url +"\n")
-	makefile.write("COMMENT = " + package.get_manifest(packageName).brief +"\n")
-	makefile.write("LICENSE = " + package.get_manifest(packageName).license +"\n\n")
-	makefile.write("NO_CONFIGURE = yes\nNO_BUILD = yes\nNO_EXTRACT = yes\n\n")
-	makefile.write("do-install:\n\t${RUN} tar -C $(ROBOTPKG_BASE) -xvf ${DISTDIR}/${DISTNAME}${EXTRACT_SUFX}\n\n")
-	
-	for depend in dependencies:
-		makefile.write("include ../../" + depend + "/depend.mk\n")
-		
-	makefile.write("include ../../mk/robotpkg.mk")
-	makefile.close()
+    folderName = package.get_path(packageName)
+    tarFileName = packageName +"-"+ version+ ".tar.gz"
+    
+    if not os.path.exists(brocrefolder+'/'+packageName):
+        os.makedirs(brocrefolder+'/'+packageName)
+    
+    print "create Makefile file"
+    makefile = open(brocrefolder+'/'+packageName+'/Makefile', 'w')
+    versionstring = "PACKAGE_VERSION"
+    makefile.write(versionstring +" = "+ version+"\n")
+    makefile.write("DISTNAME = "+ packageName+"-${"+versionstring+"}\n")
+    makefile.write("PKGNAME = "+ packageName+"-${"+versionstring+"}\n")
+    makefile.write("CATEGORIES = " + categorie + "\n")
+    makefile.write("MASTER_SITES = "+ masterSite+"\n")
+    makefile.write("MASTER_REPOSITORY = \n\n")
 
-	DESCR = open(brocrefolder+'/'+packageName+'/DESCR', 'w')
-	DESCR.write(package.get_manifest(packageName).description)
-	DESCR.close()
+    makefile.write("MAINTAINER = " + package.get_manifest(packageName).author +"\n")
+    makefile.write("HOMEPAGE = " + package.get_manifest(packageName).url +"\n")
+    makefile.write("COMMENT = " + package.get_manifest(packageName).brief +"\n")
+    makefile.write("LICENSE = " + package.get_manifest(packageName).license +"\n\n")
+    makefile.write("NO_CONFIGURE = yes\nNO_BUILD = yes\nNO_EXTRACT = yes\n\n")
+    makefile.write("do-install:\n\t${RUN} tar -C $(ROBOTPKG_BASE) -xvf ${DISTDIR}/${DISTNAME}${EXTRACT_SUFX}\n\n")
+    
+    for depend in dependencies:
+        makefile.write("include ../../" + depend + "/depend.mk\n")
+        
+    makefile.write("include ../../mk/robotpkg.mk")
+    makefile.close()
 
-	print "create depend.mk file"
-	depend_mk = open(brocrefolder+'/'+packageName+'/depend.mk', 'w')
-	depend_mk.write("DEPEND_DEPTH:=        ${DEPEND_DEPTH}+\n")
-	depend_mk.write(packageName + "_DEPEND_MK:= ${" + packageName + "_DEPEND_MK}+\n\n")
-	depend_mk.write("ifeq (+,$(DEPEND_DEPTH))\n")
-	depend_mk.write("DEPEND_PKG+=        "+packageName+"\n")
-	depend_mk.write("endif\n\n")
+    DESCR = open(brocrefolder+'/'+packageName+'/DESCR', 'w')
+    DESCR.write(package.get_manifest(packageName).description)
+    DESCR.close()
 
-	depend_mk.write("ifeq (+,$(" + packageName + "_DEPEND_MK))\n")
-	depend_mk.write("PREFER." + packageName + "?=    robotpkg\n\n")
+    print "create depend.mk file"
+    depend_mk = open(brocrefolder+'/'+packageName+'/depend.mk', 'w')
+    depend_mk.write("DEPEND_DEPTH:=        ${DEPEND_DEPTH}+\n")
+    depend_mk.write(packageName + "_DEPEND_MK:= ${" + packageName + "_DEPEND_MK}+\n\n")
+    depend_mk.write("ifeq (+,$(DEPEND_DEPTH))\n")
+    depend_mk.write("DEPEND_PKG+=        "+packageName+"\n")
+    depend_mk.write("endif\n\n")
 
-	depend_mk.write("DEPEND_USE+=        " + packageName + "\n\n")
+    depend_mk.write("ifeq (+,$(" + packageName + "_DEPEND_MK))\n")
+    depend_mk.write("PREFER." + packageName + "?=    robotpkg\n\n")
 
-	depend_mk.write("DEPEND_ABI." + packageName + "?=    " + packageName + ">=" + version + "\n")
-	depend_mk.write("DEPEND_DIR." + packageName + "?=    ../../" + brocrefolder + "/" + packageName + "\n\n")
+    depend_mk.write("DEPEND_USE+=        " + packageName + "\n\n")
 
-	depend_mk.write("SYSTEM_SEARCH." + packageName + "=\ \n")
-	depend_mk.write("    "+fileToCheckIfInstall + "\n")
-	depend_mk.write("endif\n\n")
+    depend_mk.write("DEPEND_ABI." + packageName + "?=    " + packageName + ">=" + version + "\n")
+    depend_mk.write("DEPEND_DIR." + packageName + "?=    ../../" + brocrefolder + "/" + packageName + "\n\n")
 
-	depend_mk.write("DEPEND_DEPTH:=        ${DEPEND_DEPTH:+=}\n")
-	depend_mk.close()
+    depend_mk.write("SYSTEM_SEARCH." + packageName + "=\ \n")
+    depend_mk.write("    "+fileToCheckIfInstall + "\n")
+    depend_mk.write("endif\n\n")
 
-	commands.getoutput("mkdir "+folderName+"/../"+brocrefolder)
-	commands.getoutput("ln -s "+folderName+" "+folderName+"/../"+brocrefolder+"/"+packageName)
-	print "create tarball"
-	commands.getoutput("tar -czf "+tarFileName+" --exclude-vcs -h -C"+ folderName+"/.. " +brocrefolder)
-	#commands.getoutput("tar -czf "+tarFileName+" --exclude-vcs -C"+ folderName+"/.. " +packageName)
-	
-	commands.getoutput("rm -r "+folderName+"/../"+brocrefolder)
+    depend_mk.write("DEPEND_DEPTH:=        ${DEPEND_DEPTH:+=}\n")
+    depend_mk.close()
 
-	print "create PLIST file"
-	commands.getoutput("tar -tf "+tarFileName+" | grep -P -v '/$' > "+brocrefolder+'/'+packageName+"/PLIST")
+    print "create tarball"
+    commands.getoutput("mkdir "+folderName+"/../"+brocrefolder)
+    commands.getoutput("ln -s "+folderName+" "+folderName+"/../"+brocrefolder+"/"+packageName)
+    commands.getoutput("tar -czf "+tarFileName+" --exclude-vcs -h -C"+ folderName+"/.. " +brocrefolder)
+    #commands.getoutput("tar -czf "+tarFileName+" --exclude-vcs -C"+ folderName+"/.. " +packageName)
+    commands.getoutput("rm "+folderName+"/../"+brocrefolder+"/"+packageName)
+    commands.getoutput("rmdir "+folderName+"/../"+brocrefolder)
 
-	print "compute checksums"
-	distinfo = open(brocrefolder+'/'+packageName+'/distinfo', 'w')
-	line = commands.getoutput("openssl sha1 "+ tarFileName) + "\n"
-	line = string.replace(line, "SHA1", "SHA1 ")
-	line = string.replace(line, ")=", ") =")
-	distinfo.write(line)
+    print "create PLIST file"
+    commands.getoutput("tar -tf "+tarFileName+" | grep -P -v '/$' > "+brocrefolder+'/'+packageName+"/PLIST")
 
-	line = commands.getoutput("openssl rmd160 "+ tarFileName) + "\n"
-	line = string.replace(line, "RIPEMD160", "RMD160 ")
-	line = string.replace(line, ")=", ") =")
-	distinfo.write(line)
+    print "compute checksums"
+    distinfo = open(brocrefolder+'/'+packageName+'/distinfo', 'w')
+    line = commands.getoutput("openssl sha1 "+ tarFileName) + "\n"
+    line = string.replace(line, "SHA1", "SHA1 ")
+    line = string.replace(line, ")=", ") =")
+    distinfo.write(line)
 
-	filesize = os.path.getsize(tarFileName)
-	distinfo.write("Size (" + tarFileName + ") = " + str(filesize) + " bytes")
-	distinfo.close()
+    line = commands.getoutput("openssl rmd160 "+ tarFileName) + "\n"
+    line = string.replace(line, "RIPEMD160", "RMD160 ")
+    line = string.replace(line, ")=", ") =")
+    distinfo.write(line)
 
-	print "Finished"
+    filesize = os.path.getsize(tarFileName)
+    distinfo.write("Size (" + tarFileName + ") = " + str(filesize) + " bytes")
+    distinfo.close()
+
+    print "Finished"
 
 
 if __name__ == "__main__":
-	if len(sys.argv) < 6:
-		print "Please specify the parameters:\n"
-		print "python gen_brocre_package.py <packageName> <categorie> <masterSite> <version> <brocre_folder>"
-	else:
-		parseManifest(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    if len(sys.argv) < 6:
+        print "Please specify the parameters:\n"
+        print "python gen_brocre_package.py <packageName> <categorie> <masterSite> <version> <brocre_folder>"
+    else:
+        parseManifest(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
 
 
