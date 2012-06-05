660f8ee1e84c68f3d1abeee30f534200b50f9e9e
Jan Paulus <jan.paulus@h-brs.de>
2012-06-05 16:27:11 +0200

    remove youbot-locomotec and add youbot-ros-pkg

---
+++ youbot/youbot-ros-pkg/depend.mk
@@ -0,0 +1,19 @@
+DEPEND_DEPTH:=        ${DEPEND_DEPTH}+
+youbot-ros-pkg_DEPEND_MK:= ${youbot-ros-pkg_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=        youbot-ros-pkg
+endif
+
+ifeq (+,$(youbot-ros-pkg_DEPEND_MK))
+PREFER.youbot-ros-pkg?=    robotpkg
+
+DEPEND_USE+=        youbot-ros-pkg
+
+DEPEND_ABI.youbot-ros-pkg?=    youbot-ros-pkg>=0.2
+DEPEND_DIR.youbot-ros-pkg?=    ../../youbot/youbot-ros-pkg
+
+SYSTEM_SEARCH.youbot-ros-pkg=youbot/youbot-ros-pkg/youbot_common/stack.xml
+endif
+
+DEPEND_DEPTH:=        ${DEPEND_DEPTH:+=}
