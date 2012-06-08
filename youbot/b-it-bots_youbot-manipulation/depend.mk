c5adae319ec7596d83573602233655966cb3f99a
Jan Paulus <jan.paulus@h-brs.de>
2012-06-08 12:00:07 +0200

    rename youbot_manipulation to b-it-bots_youbot_manipulation

---
+++ youbot/b-it-bots_youbot-manipulation/depend.mk
@@ -0,0 +1,19 @@
+DEPEND_DEPTH:=        ${DEPEND_DEPTH}+
+b-it-bots_youbot-manipulation_DEPEND_MK:= ${b-it-bots_youbot-manipulation_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=        b-it-bots_youbot-manipulation
+endif
+
+ifeq (+,$(b-it-bots_youbot-manipulation_DEPEND_MK))
+PREFER.b-it-bots_youbot-manipulation?=    robotpkg
+
+DEPEND_USE+=        b-it-bots_youbot-manipulation
+
+DEPEND_ABI.b-it-bots_youbot-manipulation?=    b-it-bots_youbot-manipulation>=0.1
+DEPEND_DIR.b-it-bots_youbot-manipulation?=    ../../youbot/b-it-bots_youbot-manipulation
+
+SYSTEM_SEARCH.b-it-bots_youbot-manipulation=youbot/b-it-bots_youbot-manipulation/stack.xml
+endif
+
+DEPEND_DEPTH:=        ${DEPEND_DEPTH:+=}
