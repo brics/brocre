ece67475824fa7aa71bc51c398c914cff3ca3d69
Jan Paulus <jan.paulus@h-brs.de>
2012-06-18 15:00:23 +0200

    rename brics_3d

---
+++ algorithm/brics_3d/depend.mk
@@ -0,0 +1,19 @@
+DEPEND_DEPTH:=        ${DEPEND_DEPTH}+
+brics_3d_DEPEND_MK:= ${brics_3d_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=        brics_3d
+endif
+
+ifeq (+,$(brics_3d_DEPEND_MK))
+PREFER.brics_3d?=    robotpkg
+
+DEPEND_USE+=        brics_3d
+
+DEPEND_ABI.brics_3d?=    brics_3d>=0.2.0
+DEPEND_DIR.brics_3d?=    ../../algorithm/brics_3d
+
+SYSTEM_SEARCH.brics_3d=algorithm/brics_3d/manifest.xml
+endif
+
+DEPEND_DEPTH:=        ${DEPEND_DEPTH:+=}
