721d05f6c0c9ae90e080305a4f589c502e6f43e9
Jan Paulus <jan.paulus@h-brs.de>
2012-12-05 16:52:29 +0100

    add brics_3d_bride_models

---
+++ models/brics_3d_bride_models/depend.mk
@@ -0,0 +1,19 @@
+DEPEND_DEPTH:=        ${DEPEND_DEPTH}+
+brics_3d_bride_models_DEPEND_MK:= ${brics_3d_bride_models_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=        brics_3d_bride_models
+endif
+
+ifeq (+,$(brics_3d_bride_models_DEPEND_MK))
+PREFER.brics_3d_bride_models?=    robotpkg
+
+DEPEND_USE+=        brics_3d_bride_models
+
+DEPEND_ABI.brics_3d_bride_models?=    brics_3d_bride_models>=0.1
+DEPEND_DIR.brics_3d_bride_models?=    ../../models/brics_3d_bride_models
+
+SYSTEM_SEARCH.brics_3d_bride_models=models/brics_3d_bride_models/brics_3d_bride_filtering/CMakeLists.txt
+endif
+
+DEPEND_DEPTH:=        ${DEPEND_DEPTH:+=}
