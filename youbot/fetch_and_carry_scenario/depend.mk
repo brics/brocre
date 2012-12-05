9060b64f2be92ef401fd28a33fe2b479303a86ba
Jan Paulus <jan.paulus@h-brs.de>
2012-12-05 17:24:29 +0100

    update fetch_and_carry to 0.1.1

--- youbot/fetch_and_carry_scenario/depend.mk
+++ youbot/fetch_and_carry_scenario/depend.mk
@@ -10,7 +10,7 @@ PREFER.fetch_and_carry_scenario?=    robotpkg
 
 DEPEND_USE+=        fetch_and_carry_scenario
 
-DEPEND_ABI.fetch_and_carry_scenario?=    fetch_and_carry_scenario>=0.1
+DEPEND_ABI.fetch_and_carry_scenario?=    fetch_and_carry_scenario>=0.1.1
 DEPEND_DIR.fetch_and_carry_scenario?=    ../../youbot/fetch_and_carry_scenario
 
 SYSTEM_SEARCH.fetch_and_carry_scenario=youbot/fetch_and_carry_scenario/README.md
