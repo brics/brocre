8e5d4d1015b71f29abdb3dc1984ec399bc4d1c30
Jan Paulus <jan.paulus@h-brs.de>
2013-02-18 12:29:43 +0100

    update brics_3d_bride_models

--- models/brics_3d_bride_models/depend.mk
+++ models/brics_3d_bride_models/depend.mk
@@ -10,7 +10,7 @@ PREFER.brics_3d_bride_models?=    robotpkg
 
 DEPEND_USE+=        brics_3d_bride_models
 
-DEPEND_ABI.brics_3d_bride_models?=    brics_3d_bride_models>=0.1.1
+DEPEND_ABI.brics_3d_bride_models?=    brics_3d_bride_models>=0.1.2
 DEPEND_DIR.brics_3d_bride_models?=    ../../models/brics_3d_bride_models
 
 SYSTEM_SEARCH.brics_3d_bride_models=models/brics_3d_bride_models/stack.xml
