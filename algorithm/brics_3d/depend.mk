bb1401440d7df13dd15b7e3c955c5ab6911f6beb
Frederik Hegger <frederik.hegger@h-brs.de>
2013-03-13 12:51:21 +0100

    update brics_3d

--- algorithm/brics_3d/depend.mk
+++ algorithm/brics_3d/depend.mk
@@ -10,7 +10,7 @@ PREFER.brics_3d?=    robotpkg
 
 DEPEND_USE+=        brics_3d
 
-DEPEND_ABI.brics_3d?=    brics_3d>=0.3.1
+DEPEND_ABI.brics_3d?=    brics_3d>=0.3.2
 DEPEND_DIR.brics_3d?=    ../../algorithm/brics_3d
 
 SYSTEM_SEARCH.brics_3d=algorithm/brics_3d/manifest.xml
